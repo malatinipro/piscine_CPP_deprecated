b2aa00956f76407b7626f0d7b8c0bfe1f5ae1d28
malatini <malatini@student.42.fr>
2021-10-28 10:41:51 +0200

    save

---
+++ CPP05V2/ex01/Bureaucrat.cpp
@@ -0,0 +1,69 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void): _name("Bureaucrat"), _grade(150)
+{
+  std::cout << "Default Bureaucrat constructor called" << std::endl;
+  return ;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat & src): _name(src._name), _grade(src._grade)
+{
+  std::cout << "Bureaucrat copy constructor called" << std::endl;
+  return ;
+}
+
+Bureaucrat::Bureaucrat(std::string const name, int grade): _name(name), _grade(grade)
+{
+  std::cout << "Bureaucrat constructor called" << std::endl;
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+  std::cout << "Bureaucrat is called " << this->getName() << " and has grade " << this->getGrade() << std::endl;
+  return ;
+}
+
+Bureaucrat::~Bureaucrat(void)
+{
+  std::cout << "Bureaucrat desconstructor called" << std::endl;
+  return ;
+}
+
+Bureaucrat &	Bureaucrat::operator=(Bureaucrat const & rhs)
+{
+  //(void)rhs;
+  std::cout << "Bureaucrat assignation operator called" << std::endl;
+  this->_grade = rhs._grade;
+	return *this;
+}
+
+std::ostream &  operator<<(std::ostream &COUT, Bureaucrat const & rhs)
+{
+	COUT << rhs.getName() << ", bureaucrat grade " << rhs.getGrade();
+	return COUT;
+}
+
+void	Bureaucrat::signForm(Form &form)
+{
+	try
+	{
+		form.beSigned(*this);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << this->_name << " cannot sign because ";
+		std::cerr << e.what() << std::endl;
+		return ;
+	}
+	std::cout << this->_name << " signes " << form.getName() << std::endl;
+}
+
+std::string	Bureaucrat::getName(void) const
+{
+	return(this->_name);
+}
+
+int	Bureaucrat::getGrade(void) const
+{
+	return (this->_grade);
+}
---
+++ CPP05V2/ex01/Form.hpp
@@ -0,0 +1,62 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <iostream>
+#include <string>
+
+#include "Bureaucrat.hpp"
+
+class Bureaucrat;
+
+class Form
+{
+public:
+  Form(void);
+  Form(Form const &src);
+  Form(std::string name, int gsign, int gexec);
+  Form &				operator=(Form const &rhs);
+  virtual ~Form(void);
+
+  const std::string &	      getName(void) const;
+  const int &		            getGsign(void) const;
+  const int &		            getGexec(void) const;
+  bool				              isSigned(void) const;
+  void				              beSigned(Bureaucrat const & bur);
+
+protected:
+  const std::string	      _name;
+  bool				            _signed;
+  const int     	        _gexec;
+  const int		            _gsign;
+
+  class GradeTooHighException: public std::exception
+	{
+		virtual const char * what() const throw()
+    {
+        return ("Oops ! Grade too high");
+    }
+	};
+	class GradeTooLowException: public std::exception
+	{
+		virtual const char * what() const throw()
+    {
+        return ("Oops ! Grade too low");
+    }
+	};
+  class FormNotSignedException: public std::exception
+	{
+		virtual const char * what() const throw()
+    {
+      return ("Oops ! This form is not signed yet");
+    }
+	};
+  class FormAlreadySignedException: public std::exception
+	{
+		virtual const char * what() const throw()
+    {
+      return ("Oops ! This form is already signed.");
+    }
+	};
+};
+
+#endif

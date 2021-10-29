cd1f202368cb8c1f64d099d11ed81c1744285393
malatini <malatini@student.42.fr>
2021-10-29 18:24:38 +0200

    correc makefile

--- cpp_module_07/ex00/main.cpp
+++ cpp_module_07/ex00/main.cpp
@@ -2,6 +2,20 @@
 
 int main(void)
 {
+  int a = 2;
+  int b = 3;
+  ::swap( a, b );
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+  std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+  ::swap(c, d);
+  std::cout << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+  std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+  return 0;
+  /*
   int i = 5;
   int j = 6;
 
@@ -23,4 +37,5 @@ int main(void)
   max(a, b);
   max(f, g);
   return (0);
+  */
 }
--- cpp_module_07/ex00/whatever.hpp
+++ cpp_module_07/ex00/whatever.hpp
@@ -9,50 +9,47 @@ void swap(T &first, T&second)
 {
   T tmp;
 
-  std::cout << "--------------------" << std::endl;
-  std::cout << "Swap function called" << std::endl;
-  std::cout << "--------------------" << std::endl;
+  //std::cout << "--------------------" << std::endl;
+  //std::cout << "Swap function called" << std::endl;
+  //std::cout << "--------------------" << std::endl;
   //comme c'est une référence on a presque rien besoin de faire
-  std::cout << "first is " << first << std::endl;
-  std::cout << "second is " << second << std::endl;
-  std::cout << "--------------------" << std::endl;
+  //std::cout << "first is " << first << std::endl;
+  //std::cout << "second is " << second << std::endl;
+  //std::cout << "--------------------" << std::endl;
   tmp = first;
   first = second;
   second = tmp;
-  std::cout << "first is " << first << std::endl;
-  std::cout << "second is " << second << std::endl;
-  std::cout << "--------------------" << std::endl;
+  //std::cout << "first is " << first << std::endl;
+  //std::cout << "second is " << second << std::endl;
 }
 
 template<typename T>
 T const &min(T const &one, T const &second)
 {
-  std::cout << "--------------------" << std::endl;
-  std::cout << "min function called" << std::endl;
-  std::cout << "--------------------" << std::endl;
+  //std::cout << "--------------------" << std::endl;
+  //std::cout << "min function called" << std::endl;
+  //std::cout << "--------------------" << std::endl;
   if (one < second)
   {
-    std::cout << "the min is " << one << std::endl;
+    //std::cout << "the min is " << one << std::endl;
     return (one);
   }
-  std::cout << "the min is " << second << std::endl;
+  //std::cout << "the min is " << second << std::endl;
   return (second);
-  std::cout << "--------------------" << std::endl;
 }
 
 template<typename T>
 T const &max(T const &one, T const &second)
 {
-  std::cout << "--------------------" << std::endl;
-  std::cout << "max function called" << std::endl;
-  std::cout << "--------------------" << std::endl;
+  //std::cout << "--------------------" << std::endl;
+  //std::cout << "max function called" << std::endl;
+  //std::cout << "--------------------" << std::endl;
   if (one > second)
   {
-    std::cout << "the max is " << one << std::endl;
+    //std::cout << "the max is " << one << std::endl;
     return (one);
   }
-  std::cout << "the max is " << second << std::endl;
-  std::cout << "--------------------" << std::endl;
+  //std::cout << "the max is " << second << std::endl;
   return (second);
 }
 

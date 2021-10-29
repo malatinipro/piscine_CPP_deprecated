1bf2a8b90477da7a856e7aa7ef1bc466f1e20606
malatini <malatini@student.42.fr>
2021-10-29 20:25:39 +0200

    save

--- cpp_module_07/ex02/Array.hpp
+++ cpp_module_07/ex02/Array.hpp
@@ -13,27 +13,16 @@ class Array
 public:
   //Constructeur sans paramettre
   //cree un empty array
-  Array(void): _arr(NULL), _size(0) {}
+  Array(): _arr(NULL), _size(0) {;}
   //Constructeur avec paramettre
   //https://en.cppreference.com/w/cpp/language/default_initialization
-  Array(unsigned int n): _arr(new T[n]), _size(n)
-  {
-    /* Pas besoin de ça pour l'initialisation par defaut
-    int i = 0;
-    while (i < n)
-    {
-      //Je mets 0 par défaut
-      this->_arr[i] = 0;
-      i++;
-    }
-    */
-  }
+  Array(unsigned int n): _arr(new T[n]), _size(n) {;}
   //Constructeur par copie
   Array(Array const &src)
   {
     //Deep copying the array
     this->_size = src._size;
-    this->arr = new T[this->_size];
+    this->_arr = new T[this->_size];
     //initialisation par defaut
     int i = 0;
     while (i < this->_size)
@@ -42,41 +31,41 @@ public:
       i++;
     }
     return (*this);
-
+  }
     //Destructeur
-    ~Array(void)
-    {
-      if (this->_arr)
-        delete [] this->_arr;
-    }
-
+  ~Array()
+  {
+    if (this->_arr)
+      delete [] this->_arr;
+  }
     //Operateur d'assignation
-    Array &operator=(Array const &src)
+  Array &operator=(Array const &src)
+  {
+    if (this->_size != 0 && this->_size != src._size)
     {
-      if (this->size != 0 && this->size != src._size)
-      {
-        delete [] this->_array;
-      }
-      //on doit refaire le tableau
-      this->_size = src._size;
-      this->_array = new T[this->_size];
-      int i = 0;
-      while (i < this->_size)
-      {
-        this->_arr[i] = src._arr[i];
-        i++;
-      }
-      return (*this);
+      delete [] this->_array;
     }
-
-    //Surcharge d'operateur demandé dans le sujet
-    T &operator[](unsigned int const &index)
+    //on doit refaire le tableau
+    this->_size = src._size;
+    this->_array = new T[this->_size];
+    int i = 0;
+    while (i < this->_size)
     {
-      if (index >= this->size)
-        throw std::exception::OutofRangeException();
-      else
-        return (this->_arr[index]);
+      this->_arr[i] = src._arr[i];
+      i++;
     }
+    return (*this);
+  }
+  //Surcharge d'operateur demandé dans le sujet
+  T &operator[](unsigned int const &index)
+  {
+    if (index >= this->_size)
+      //https://newbedev.com/how-to-throw-std-exceptions-with-variable-messages
+      //https://stackoverflow.com/questions/12261915/how-to-throw-stdexceptions-with-variable-messages
+      throw std::runtime_error(std::string("Out of range !"));
+    else
+      return (this->_arr[index]);
+  }
 
 private:
   T             *_arr;
--- cpp_module_07/ex02/main.cpp
+++ cpp_module_07/ex02/main.cpp
@@ -1,1 +1,68 @@
 #include "Array.hpp"
+#include <iostream>
+
+int main(void)
+{
+  //Instanciation de ma classe template
+  std::cout << "------------- Testing strings ---------------" << std::endl;
+  std::cout << "Instanciating type std::string" << std::endl;
+  Array <std::string> str(6);
+
+  std::string hello = "Hello!";
+  int i = 0;
+  std::cout << "Setting the values in the array" << std::endl;
+  while (i < 5)
+  {
+    str[i] = hello[i];
+    std::cout << hello[i] << std::endl;
+    i++;
+  }
+  str[i] = '\0';
+  i = 0;
+  std::cout << "Accessing the values using operator []" << std::endl;
+  while (i < 5)
+  {
+    std::cout << str[i] << std::endl;
+    i++;
+  }
+  std::cout << "Testing if we go out of range" << std::endl;
+  try
+  {
+    std::cout << str[10] << std::endl;
+  }
+  catch (std::exception &e)
+  {
+      std::cout << e.what() << std::endl;
+  }
+  std::cout << "-----------------------------------" << std::endl;
+  std::cout << "------------- Testing ints ---------------" << std::endl;
+  std::cout << "Instanciating type int" << std::endl;
+  Array <int> tab(3);
+  std::cout << "Setting the values in the array" << std::endl;
+  int src[3] = {1, 2, 3};
+  i = 0;
+  while (i < 3)
+  {
+    tab[i] = src[i];
+    std::cout << src[i] << std::endl;
+    i++;
+  }
+  std::cout << "Accessing the values using operator []" << std::endl;
+  i = 0;
+  while (i < 3)
+  {
+    std::cout << tab[i] << std::endl;
+    i++;
+  }
+  std::cout << "Testing if we go out of range" << std::endl;
+  try
+  {
+    std::cout << tab[10] << std::endl;
+  }
+  catch (std::exception &e)
+  {
+    std::cout << e.what() << std::endl;
+  }
+  std::cout << "-----------------------------------" << std::endl;
+  return (0);
+}

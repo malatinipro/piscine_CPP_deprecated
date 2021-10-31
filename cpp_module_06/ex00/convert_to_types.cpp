fac715669d2e56f2915c62f8b4745b0f049d3bda
malatini <malatini@student.42.fr>
2021-10-31 14:46:16 +0100

    correc premier exo

--- cpp_module_06/ex00/convert_to_types.cpp
+++ cpp_module_06/ex00/convert_to_types.cpp
@@ -22,27 +22,27 @@ void display_char(std::string const &str)
 
 bool display_int(std::string const &str)
 {
-  //std::cout << "int display function called" << std::endl;
-  //strtol converts string to long integer
-  //data va permettre de convertir la string en char * (tableau)
-	long int	l = strtol(str.data(), NULL, 10);
+	long long	l = strtol(str.data(), NULL, 10);
 	int			i = 0;
 
+  (void)l;
+  /*
 	if (l > INT_MAX || l < INT_MIN)
 	{
 		std::cerr << "Error: invalid value" << std::endl;
 		return (false);
 	}
+  */
 	i = atoi(str.data());
 	if (i > CHAR_MAX || i < CHAR_MIN)
-		std::cout << "char: not ascii" << std::endl;
+		std::cout << "char: Non displayable" << std::endl;
   else if (!isprint(static_cast<char>(i)))
-  	std::cout << "char: not printable" << std::endl;
+  	std::cout << "char: Non displayable" << std::endl;
 	else
 		std::cout << "char: " << static_cast<char>(i) << std::endl;
 	std::cout << "int: " << i << std::endl;
-	std::cout << "float: " << std::setprecision(6) << static_cast<float>(i) << std::endl;
-	std::cout << "double: " << std::setprecision(6) << static_cast<double>(i) << std::endl;
+	std::cout << "float: " << static_cast<float>(i) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(i) << ".0" << std::endl;
 	return (true);
 }
 
@@ -55,13 +55,12 @@ bool	display_float(std::string const & str)
   //std::cout << "float display function called" << std::endl;
   if (str == s1 || str == s2 || str == s3)
   {
-    std::cout << "char: not ascii" << std::endl;
-    std::cout << "int: out of range" << std::endl;
-    std::cout << "float: " << std::setprecision(6) << str << std::endl;
-    std::cout << "double: " << std::setprecision(6) << str.substr(0, str.length() - 1) << std::endl;
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: " << str << std::endl;
+    std::cout << "double: " << str.substr(0, str.length() - 1) << std::endl;
     return (true);
   }
-  //Attention checker
 	double	d = strtod(str.data(), NULL);
 	float	f;
 	(void)d;
@@ -73,16 +72,15 @@ bool	display_float(std::string const & str)
   //conversion en float
 	f = atof(str.data());
 	if (static_cast<int>(f) > CHAR_MAX || static_cast<int>(f) < CHAR_MIN)
-		std::cout << "char: not ascii" << std::endl;
+		std::cout << "char: Non displayable" << std::endl;
   else if (!isprint(static_cast<char>(f)))
-    std::cout << "char: not printable" << std::endl;
+    std::cout << "char: Non displayable" << std::endl;
 	else
 		std::cout << "char: " << static_cast<char>(f) << std::endl;
 	if (static_cast<long>(f) > INT_MAX || static_cast<long>(f) < INT_MIN)
 		std::cout << "int: out of range" << std::endl;
 	else
 		std::cout << "int: " << static_cast<int>(f) << std::endl;
-  //Check de la precision
   if (fmod(f, 1))
   {
   	std::cout << "float: " << f << "f" << std::endl;
@@ -90,8 +88,8 @@ bool	display_float(std::string const & str)
   }
   else
   {
-  	std::cout << "float: " << f << ".0f" << std::endl;
-  	std::cout << "double: " << static_cast<double>(f) << ".0" << std::endl;
+    std::cout << "float: " << f << ".0f" << std::endl;
+	  std::cout << "double: " << static_cast<double>(f) << ".0" << std::endl;
   }
 	return (true);
 }
@@ -119,9 +117,9 @@ bool	display_double(std::string const & str)
 		return (false);
 	}
 	if (static_cast<int>(d) > CHAR_MAX || static_cast<int>(d) < CHAR_MIN)
-		std::cout << "char: not ascii" << std::endl;
+		std::cout << "char: Non displayable" << std::endl;
   else if (!isprint(static_cast<char>(d)))
-    std::cout << "char: not printable" << std::endl;
+    std::cout << "char: Non displayable" << std::endl;
 	else
 		std::cout << "char: " << static_cast<char>(d) << std::endl;
 	if (static_cast<long>(d) > INT_MAX || static_cast<long>(d) < INT_MIN)
@@ -135,10 +133,11 @@ bool	display_double(std::string const & str)
   }
   else
   {
-  	std::cout << "float: " << d << ".0f" << std::endl;
-  	std::cout << "double: " << static_cast<double>(d) << ".0" << std::endl;
+    std::cout << "float: " << d << ".0f" << std::endl;
+    std::cout << "double: " << static_cast<double>(d) << ".0" << std::endl;
   }
-	return (true);
+  return (true);
+	
 }
 
 bool  convert_to_types(std::string const &str)
--- cpp_module_06/ex00/main.cpp
+++ cpp_module_06/ex00/main.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <cmath>
 
-//checker sur Linux
 #define ERROR 1
 
 bool  check_type(std::string const &str);
@@ -17,22 +16,12 @@ int main(int argc, char **argv)
     std::cerr << "Error: wrong numbers of arguments" << std::endl;
     return (ERROR);
   }
-  //checker qu'il n'y a pas de pb sur Linux
   arg = argv[1];
-  //std::cout << "arg is " << arg << std::endl;
   if (arg.empty())
   {
     std::cerr << "Error: the string arg is empty." << std::endl;
     return (ERROR);
   }
   convert_to_types(arg);
-  //1. Verifier que la valeur appartient a un type scalaire
-    // = détecter le type du littéral
-    //puis acquérir ce littéral dans le bon type
-    //types scalaires suivants : char, int, float ou double.
-  //2. Vérifier les valeurs pour chaque type, renvoyer des erreurs
-    //pour les floats et les doubles il faut accepter les "pseudos littéraux"
-  //3. Faire la conversion explicitement dans les trois types
-  // ET afficher avec le formattagea
   return (0);
 }
